68fab4abe8a19ada46f0e19495219758193c39a0
Ivan Litovchenko <ivan.litovchenko@beetroot.com.ua>
2020-03-23 18:17:15 +0200

    init

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'tsconfig.json_old',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
